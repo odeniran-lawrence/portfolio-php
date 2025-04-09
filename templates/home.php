755ef333d850306cc6253fe8b58890334c21e06a
odeniran-lawrence <odelaw9@gmail.com>
2025-04-09 09:50:14 +0200

    Bento style

--- templates/home.php
+++ templates/home.php
@@ -3,23 +3,22 @@
 
     <div class="grid grid-cols-9 grid-rows-9 gap-4  w-[700px] h-[600px] m-auto my-5">
 
-        <div class="col-span-3 row-span-5"><img src="../images/home_1.png" alt="" class="w-full h-full object-cover"></div>
+        <div class="bg-slate-200 col-span-3 row-span-5">TestTTTT</div>
 
-  
+        <div class="bg-slate-200 col-span-4 row-span-4 col-start-1 row-start-6">test</div>
 
-        <div class="col-span-4 row-span-4 col-start-1 row-start-6"><img src="../images/home_3.png" alt="" class="w-full h-full object-cover"></div>
+        <div class="bg-slate-200 row-span-2 col-start-4 row-start-4">TEST</div>
 
-        <div class="row-span-2 col-start-4 row-start-4"></div>
+        <a href="/contact" class="bg-slate-200 col-span-5 row-span-3 col-start-4 row-start-1">
+            <p class="">Me contacter</p>
+        </a>
 
-        <div class="col-span-5 row-span-3 col-start-4 row-start-1"><img src="../images/home_2.png" alt="" class="w-full h-full object-cover">5</div>
+        <div class="bg-slate-200 col-span-4 row-span-6 col-start-5 row-start-4">ttttest</div>
 
-        <div class="col-span-4 row-span-6 col-start-5 row-start-4"><img src="../images/home_1.png" alt="" class="w-full h-full object-cover"></div>
+        <div class="bg-slate-200 row-span-9 col-start-9 row-start-1 border-4 border-indigo-500">
+            <a href="/about">A propos</a>
+        </div>
 
-        <div class="row-span-9 col-start-9 row-start-1 border-4 border-indigo-500">
-            <a href="http://"></a></div>
-            <a href="http://"></a></div>
-            <a href="http://"></a></div>
-            <a href="http://"></a></div>
 
     </div>
 </section>
